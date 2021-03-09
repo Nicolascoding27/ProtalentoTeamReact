a9b8d650a27feb26f0185ff55e9ff117af49cf77
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-03-08 20:40:46 -0500

    state added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {robots} from './people'
 import Cardlist from './CardList'
 import SearchBox from './Searchbox.js'
+// import { Component } from 'react'; se pasa arriba
+//STATE >> props //state means the description of the app, is an objec that describe 
+//the app
+//we are able to change states, props are things thatg come out from state
+//START TO USING STATE
 
-const App= () =>{
-    return(
-        <div class="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox/>
-        <Cardlist robots={robots} />
-        </div>
-    ) 
+class App extends  Component{
+    constructor(){
+        super()//calls the constructor of component
+        this.state={
+                robots:robots,
+                searchfield:''
+        }
+    }
+    render(){
+        return(
+            <div class="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox/>
+            <Cardlist robots={this.state.robots} />
+            </div>
+        ) 
+    }
+    
 
 }
 export default App;
